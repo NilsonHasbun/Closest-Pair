dbf90ca6e8374d4d2d3df1270a06d2efc5ac09ec
Hasbunn <78665594+NilsonHasbun@users.noreply.github.com>
2022-11-21 08:22:38 -0500

    Add files via upload
    Se cambio de lugar la clase punto

---
+++ Puntoxy.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punto;
+
+/**
+ *
+ * @author nilsi
+ */
+public class Puntoxy {
+
+   public static class Punto {
+
+        public int x;
+        public int y;
+
+        Punto(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
